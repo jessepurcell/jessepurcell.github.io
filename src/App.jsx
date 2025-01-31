d312598df976f6f476d2b29a421953ecb8af0147
Jesse Purcell <jesse.purcell@my.edu.au>
2025-01-31 19:49:47 +1000

    Vite React Tailwind Test

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SideBar from './SideBar'
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <SideBar/>
+  )
+}
+
+export default App
