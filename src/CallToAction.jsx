33c5c60dc1c75f734f6b62bb874a7165a6b682cb
Jesse Purcell <slaydevil@hotmail.com>
2025-02-02 22:08:49 +1000

    Fix spelling inconsistencies

--- src/CallToAction.jsx
+++ src/CallToAction.jsx
@@ -2,7 +2,7 @@ const CallToAction = () => {
     return (
         <section className="bg-background text-gray-200 text-center py-16">
             <div className="max-w-6xl mx-auto">
-                <h1 className="text-4xl font-bold">Private, Encrypts Email & Messaging</h1>
+                <h1 className="text-4xl font-bold">Private, Encrypted Email & Instant Messaging</h1>
                 <h2 className="text-3xl font-bold">No Ads. No Tracking.</h2>
                 <p className="mt-4 text-lg">Communicate freely with end-to-end encryption and total privacy.</p>
                 <button class="px-8 py-6 mt-4 text-2xl bg-accent text-gray-800 rounded-4xl hover:bg-gray-100 font-bold">
