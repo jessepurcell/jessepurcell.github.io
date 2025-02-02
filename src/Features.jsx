0b8ab5dd278881b6a0b73045913481b4f28aa9fd
Jesse Purcell <slaydevil@hotmail.com>
2025-02-02 20:50:15 +1000

    Update padding

--- src/Features.jsx
+++ src/Features.jsx
@@ -3,7 +3,7 @@ const Features = () => {
         <section className="py-16 bg-background">
             <div className="max-w-6xl mx-auto text-center">
                 <h2 className="text-3xl font-bold text-gray-200">Why choose us?</h2>
-                <div className="mt-16 grid md:grid-cols-3 gap-12">
+                <div className="m-8 mt-16 grid md:grid-cols-3 gap-12">
                     <div className="p-6 bg-primary shadow-2xl rounded-3xl">
                         <h3 className="text-gray-200 text-xl font-semibold">End-to-End Encryption</h3>
                         <p className="text-gray-300 mt-2">Secure messaging and email</p>
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
-        <footer className="bg-primary text-gray-800 text-center py-4 fixed bottom-0 w-full">
-            <p>Copyright &copy; 2025 Alphabeta</p>
+        <footer className="bg-primary text-white text-center py-4 fixed bottom-0 w-full">
+            <p>Copyright &copy; {new Date().getFullYear()} Alphabeta</p>
         </footer>
     );
 }
